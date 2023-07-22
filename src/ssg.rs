6d8d7ca2f2c2c280d9f9189b793afbda75d9c82d
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-07-22 12:10:34 +0700

    chore: format of html header

--- src/ssg.rs
+++ src/ssg.rs
@@ -34,23 +34,23 @@ pub fn init_project() -> Result<(), std::io::Error> {
     let header_content = format!(
         "<!DOCTYPE html>
         <html lang=\"en\">
-        <head>
-            <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
-            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
-            <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />
-            <meta http-equiv=\"cache-control\" content=\"no-cache, must-revalidate, post-check=0, pre-check=0\" />
-            <meta http-equiv=\"cache-control\" content=\"max-age=0\" />
-            <meta http-equiv=\"expires\" content=\"0\" />
-            <meta http-equiv=\"expires\" content=\"Tue, 01 Jan 1980 1:00:00 GMT\" />
-            <meta http-equiv=\"pragma\" content=\"no-cache\" />
-            <link rel=\"stylesheet\" href=\"./assets/styles.css\" />
-            <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"./assets/favicon.png\" />
-            <title>{}</title>
-            <meta name=\"description\" content=\"{}\" />
-            <meta name=\"author\" content=\"{}\" />
-            <meta name=\"keywords\" content=\"{}\" />
-            <!-- Add more fields here as needed -->
-        </head>
+            <head>
+                <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
+                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+                <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />
+                <meta http-equiv=\"cache-control\" content=\"no-cache, must-revalidate, post-check=0, pre-check=0\" />
+                <meta http-equiv=\"cache-control\" content=\"max-age=0\" />
+                <meta http-equiv=\"expires\" content=\"0\" />
+                <meta http-equiv=\"expires\" content=\"Tue, 01 Jan 1980 1:00:00 GMT\" />
+                <meta http-equiv=\"pragma\" content=\"no-cache\" />
+                <link rel=\"stylesheet\" href=\"./assets/styles.css\" />
+                <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"./assets/favicon.png\" />
+                <title>{}</title>
+                <meta name=\"description\" content=\"{}\" />
+                <meta name=\"author\" content=\"{}\" />
+                <meta name=\"keywords\" content=\"{}\" />
+                <!-- Add more fields here as needed -->
+            </head>
         </html>",
         title, description, author, keywords
     );
